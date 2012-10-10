621fe00556c196a8ff0d3814444a7b101ce5eb7c
Ken Brucker <ken@pumastudios.com>
2012-10-10 08:15:48 -0700

    Cleanup TODO list

--- class-hrecipe-food-db.php
+++ class-hrecipe-food-db.php
@@ -25,8 +25,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 
-// FIXME Handle Re-activation of plugin when database files already exist
-
 // Protect from direct execution
 if (!defined('WP_PLUGIN_DIR')) {
 	header('Status: 403 Forbidden');
